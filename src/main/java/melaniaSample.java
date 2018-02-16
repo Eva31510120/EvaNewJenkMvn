32bfe8bc166a691f7976978e934b13ba0d42a731
Eva31510120 <velnovar@gmail.com>
2018-02-16 09:47:29 -0800

    New commit today

--- src/main/java/melaniaSample.java
+++ src/main/java/melaniaSample.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class melaniaSample {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) //throws InterruptedException
+     {
         System.setProperty("webdriver.chrome.driver", "/Applications/Drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
-        Thread.sleep(5000);
+        //Thread.sleep(5000);
 
         driver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("Melania Trump");
         driver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).submit();
